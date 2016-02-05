0789c7069d1e56f637e2b99143e3ab2bd3c493da
Christine Brass <cybrass@gmail.com>
2016-02-04 23:12:16 -0500

    Second part of fizzbuzz

--- functions/fizzbuzz.exs
+++ functions/fizzbuzz.exs
@@ -9,3 +9,17 @@ IO.puts fizzbuzz.(1, 2, 3)  # should output 3
 IO.puts fizzbuzz.(1, 0, 1)  # should output "Buzz"
 IO.puts fizzbuzz.(0, 0, 1)  # should output "FizzBuzz"
 IO.puts fizzbuzz.(0, 1, 1)  # should "Fizz"
+
+IO.puts "----""
+
+call_fizzbuzz = fn
+  n -> fizzbuzz.(rem(n, 3), rem(n, 5), n)
+end
+
+IO.puts call_fizzbuzz.(10)
+IO.puts call_fizzbuzz.(11)
+IO.puts call_fizzbuzz.(12)
+IO.puts call_fizzbuzz.(13)
+IO.puts call_fizzbuzz.(14)
+IO.puts call_fizzbuzz.(15)
+IO.puts call_fizzbuzz.(16)
